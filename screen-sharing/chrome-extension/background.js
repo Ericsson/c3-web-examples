9b784cca976310e4d555b290059a838129040d4b
Negar Fazeli <negar.fazeli@ericsson.com>
2016-10-25 12:01:50 +0200

    Add chrome extension

---
+++ screen-sharing/chrome-extension/background.js
@@ -0,0 +1,34 @@
+﻿var data_sources = ['screen', 'window'],
+    desktopMediaRequestId = '';
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (msg) {
+        if (msg.message === 'requestChromeMediaSourceId') {
+            requestScreenSharing(port, msg);
+        }
+        if (msg.message === 'getInstalledStatus') {
+            isInstalled(port, msg);
+        }
+        if (msg.message === 'extensionId') {
+            extentionIsInstalled(port, msg);
+        }
+    });
+});
+
+function isInstalled(port, msg) {
+    msg.extensionId = chrome.runtime.id;
+    msg.message = 'extensionIsInstalled';
+    port.postMessage(msg);
+}
+
+function requestScreenSharing(port, msg) {
+    desktopMediaRequestId = chrome.desktopCapture.chooseDesktopMedia(
+        data_sources, port.sender.tab, function (streamId) {
+        if (streamId) {
+            msg.extensionId = chrome.runtime.id;
+            msg.message = 'chromeMediaSourceId';
+            msg.content = streamId
+        }
+        port.postMessage(msg);
+    });
+}
---
+++ screen-sharing/chrome-extension/content-script.js
@@ -0,0 +1,12 @@
+﻿var port = chrome.runtime.connect(chrome.runtime.id);
+
+port.onMessage.addListener(function (msg) {
+    window.postMessage(msg, '*');
+});
+
+window.addEventListener('message', function (event) {
+    if (event.source != window) return;
+    if (event.data) {
+        port.postMessage(event.data);
+    }
+}, false);
